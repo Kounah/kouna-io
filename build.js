848b163301a43175c85d0223fd404de921d62461
Kounah <kounalyn@gmail.com>
2018-05-25 12:37:26 +0200

    added build.js

---
+++ build.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const {execSync} = require('child_process');
+const process = require('process');
+const color = require('colors');
+const shell = require('shelljs')
+
+const dir = path.join(process.argv[1], '..');
+const dist = path.join(dir, 'dist');
+
+if(!fs.existsSync(dist)) shell.mkdir('-p', dist);
+
+console.log('building server to' + ('' + dist).cyan);
+
+execSync(`webpack ${path.join(dir, 'src', 'js', 'index.js')} --target="node" --mode production --output ${path.join(dist, 'main.js')}`, (err, stdout, stderr) => {
+  console.log(stdout);
+}):
