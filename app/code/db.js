9a54391301c2a5a31c10020b0b653eec74e7f204
Kevin Fochtler <kf@4fo.de>
2018-07-20 09:45:47 +0200

    now deprecated will be removed soon

--- app/code/db.js
+++ app/code/db.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const User = require('../models/user');
@@ -5,6 +6,10 @@ const Document = require('../models/document');
 const BnsChar = require('../models/bnschar');
 const BnsRaid = require('../models/bnsraid');
 const Invite = require('../models/invite');
+
+const colors = require('colors');
+
+console.log('[deprecated warning]'.yellow, 'please require the models module directly from its module folder [app/models]', new Error().stack);
 
 module.exports = {
   User,
